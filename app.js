aeaf7023ca77aab61b44d2f73da099e81046b415
didik <didiklsto4u@gmail.com>
2025-05-13 10:41:55 +0700

    update test eror database

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.get('/app2', (req, res) => {
 });
 
 app.get('/users', (req, res, next) => {
-  const sql = "SELECT * FRO tb_data ORDER BY id desc"
+  const sql = "SELECT * FROM tb_data ORDER BY id desc"
   connection.query(sql,(error, fields) => {
     if (error) {
       console.log('error', error)
